28e2f9341682304511c8a8beeedde61aac3af34d
ericpkatz <ericpkatz@gmail.com>
2022-10-15 12:43:23 -0400

    setup routes and tests

--- test/spec.js
+++ test/spec.js
@@ -1,5 +1,7 @@
 const { expect } = require('chai');
 const db = require('../db');
+const supertest = require('supertest');
+const app = supertest(require('../app'));
 
 describe('addition', ()=> {
   describe('1 + 1', ()=> {
@@ -20,5 +22,21 @@ describe('My App', ()=> {
     const actors = await db.models.Actor.findAll();
     expect(actors.length).to.equal(5);
 
+  });
+  describe('API', ()=> {
+    describe('/api/movies', ()=> {
+      it('returns the movies', async()=> {
+        const response = await app.get('/api/movies');
+        expect(response.status).to.equal(200);
+        expect(response.body.length).to.equal(4);
+      });
+    });
+    describe('/api/actors', ()=> {
+      it('returns the actors', async()=> {
+        const response = await app.get('/api/actors');
+        expect(response.status).to.equal(200);
+        expect(response.body.length).to.equal(5);
+      });
+    });
   });
 });
