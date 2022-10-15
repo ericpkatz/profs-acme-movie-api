0030ab58220caead24a5d575079fbadc8f84d137
ericpkatz <ericpkatz@gmail.com>
2022-10-15 12:30:11 -0400

    add sequelize

--- test/spec.js
+++ test/spec.js
@@ -1,9 +1,24 @@
 const { expect } = require('chai');
+const db = require('../db');
 
 describe('addition', ()=> {
   describe('1 + 1', ()=> {
     it('equals 3', ()=> {
       expect(1 + 1).to.equal(2);
     });
+  });
+});
+
+describe('My App', ()=> {
+  beforeEach(async()=> {
+    await db.syncAndSeed();
+  });
+  it('can sync', ()=> {
+
+  });
+  it('there are 5 actors', async()=> {
+    const actors = await db.models.Actor.findAll();
+    expect(actors.length).to.equal(5);
+
   });
 });
